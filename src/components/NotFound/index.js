67dc83c7137128aebc3fae8b1a1669d952bfc9a0
sowRaj2023 <sownyuvan1180@gmail.com>
2024-05-02 16:16:43 +0530

    commit message

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -6,7 +6,9 @@ import "./index.css"
 const NotFound = () => (
     <>
     <Header />
-    <h1 className="notfound">Not Found</h1>
+    <div className="notfound">
+        <img src="https://img.freepik.com/free-vector/hand-drawn-no-data-concept_52683-127829.jpg" alt="notfound" />
+    </div>
     </>
 
 )
